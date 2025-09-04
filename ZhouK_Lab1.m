57eb4b800b774b0d98aebf527b0f4152d2c41410
Kevin Zhou <kzhou40@stuy.edu>
2025-09-03 22:29:20 -0400

    fixed comments

--- ZhouK_Lab1.m
+++ ZhouK_Lab1.m
@@ -1,20 +1,25 @@
 function output = ZhouK_Lab1(ary)
 
-arySize = size(ary); % row vector containing the size of the input array
+% row vector containing the size of the input array
+arySize = size(ary); 
 
 % statement to set the output to 0 if the array isn't square
 if arySize(1)~=arySize(2);
     output = 0;
 else
     % START OF FIRST OUTPUT VALUE
+    
+    % row vector containing the max value of each column
+    mostPositive = max(ary) 
+    
+    % row vector containing the min value of each row, which is done by transposing the matrix prior to calling min
+    mostNegative = min(transpose(ary)); 
 
-    mostPositive = max(ary) % row vector containing the max value of each column
-
-    mostNegative = min(transpose(ary)); % row vector containing the min value of each row, which is done by transposing the matrix prior to calling min
     % the flip command is used to reverse the order of the vector, then it is flipped again to become a column vector
     mostNegative = transpose(flip(mostNegative))
 
-    firstValue = dot(mostPositive,mostNegative); % the first output value is the dot product of the vectors
+    % the first output value is the dot product of the vectors
+    firstValue = dot(mostPositive,mostNegative); 
 
     % END OF FIRST OUTPUT VALUE
 
@@ -25,24 +30,30 @@ else
     % START OF FIRST VECTOR TO BE DOTTED
 
     % Largest magnitude term in each column
-    [M,I] = max(abs(ary)); % Use this syntax of the max function in order to store the data for the row numbers of the largest magnitudes of each column of the matrix
-    largestMag = []; % define an empty column vector
+    % Use this syntax of the max function in order to store the data for the row numbers of the largest magnitudes of each column of the matrix
+    [M,I] = max(abs(ary)); 
+    % define an empty column vector
+    largestMag = []; 
     % iterate for each column of the matrix
     for c=1:arySize(1)
-        largestMag = cat(1,largestMag,[ary(I(c),c)]); % concatenate to the bottom of column vector another column vector containing the row index from the variable I, and the column index from the for loop variable
+        % concatenate to the bottom of column vector another column vector containing the row index from the variable I, and the column index from the for loop variable
+        largestMag = cat(1,largestMag,[ary(I(c),c)]); 
     end
 
     % Most negative term in each column
-    mostNegative = transpose(min(ary)); % first take the min of all elements, then transpose the returned row vector into a column vector
+    % first take the min of all elements, then transpose the returned row vector into a column vector
+    mostNegative = transpose(min(ary)); 
 
     % Magnitudes of the sort terms
     mostNegative = abs(mostNegative);
 
     % Concatenate them, with the sort terms first
-    catVector = cat(2,mostNegative,largestMag); % concatenate the vectors by appending them as columns, in the correct order
+    % concatenate the vectors by appending them as columns, in the correct order
+    catVector = cat(2,mostNegative,largestMag); 
 
     % Sort descending by the first column
-    catVector = sortrows(catVector,'descend'); % call the sortrows command, specifying descending order
+    % call the sortrows command, specifying descending order
+    catVector = sortrows(catVector,'descend'); 
 
     % First vector to be dotted
     firstVec = catVector(:,2)
@@ -52,11 +63,14 @@ else
     % START OF SECOND VECTOR TO BE DOTTED
 
     % Smallest magnitude term in each row
-    [M,I] = min(abs(transpose(ary))); % Use this syntax of the min function in order to store the data for the column numbers of the largest magnitudes of each row of the matrix
-    smallestMag = []; % define an empty column vector
+    % Use this syntax of the min function in order to store the data for the column numbers of the largest magnitudes of each row of the matrix
+    [M,I] = min(abs(transpose(ary))); 
+    % define an empty column vector
+    smallestMag = []; 
     % iterate for each column of the matrix
     for c=1:arySize(1)
-        smallestMag = cat(1,smallestMag,[ary(c,I(c))]); % concatenate to the bottom of column vector another column vector containing the column index from the variable I, and the row index from the for loop variable.
+        % concatenate to the bottom of column vector another column vector containing the column index from the variable I, and the row index from the for loop variable.
+        smallestMag = cat(1,smallestMag,[ary(c,I(c))]); 
     end
 
     % Diagonal term in each row
@@ -66,15 +80,18 @@ else
     diagonalTerms = abs(diagonalTerms);
 
     % Concatenate them with the sort terms first
-    catVector = cat(2,diagonalTerms,smallestMag); % concatenate the vectors by appending them as columns, in the correct order
+    % concatenate the vectors by appending them as columns, in the correct order
+    catVector = cat(2,diagonalTerms,smallestMag); 
 
     % Sort ascending by the first column
-    catVector = sortrows(catVector); % call the sortrows command
+    % call the sortrows command
+    catVector = sortrows(catVector); 
 
     % Second vector to be dotted
     secondVec = catVector(:,2)
-
-    secondValue = dot(firstVec, secondVec); % the second output value is the dot product of the two vectors
+    
+    % the second output value is the dot product of the two vectors
+    secondValue = dot(firstVec, secondVec); 
 
     % END OF SECOND VECTOR TO BE DOTTED
 
@@ -83,24 +100,30 @@ else
     %------------------------------------------------------------------------------------
 
     % START OF THIRD OUTPUT VALUE
-
-    thirdValue = 1; % counter variable that assumes that the matrix is positive definite
-
-    index = 1; % initialie the counter variable
+    
+    % assume that the matrix is positive definite
+    thirdValue = 1; 
+    
+    % initialie the index variable
+    index = 1; 
 
     % this while loop will only run when the matrix has not yet found to be not positive definite and when the current size of the submatrix being checked is still less than or equal to the size of the entire matrix
     while (thirdValue && index <= arySize(1))
-        subMatrix = ary(1:index,1:index); % iniitalize the submatrix
+        % iniitalize the submatrix with the proper size
+        subMatrix = ary(1:index,1:index); 
         if det(subMatrix) <= 0
-            thirdValue = 0; % update the counter if the submatrix is not positive definite
+            % update the counter if the submatrix is found to not be positive definite
+            thirdValue = 0; 
         end
-        index = index + 1; % increment the index to check the next submatrix
+        % increment the index to check the next submatrix
+        index = index + 1; 
     end
 
     % END OF THIRD OUTPUT VALUE
 
     %------------------------------------------------------------------------------------
 
-    output = [firstValue; secondValue; thirdValue]; % Combine the output values into a single column vector
+    % Combine the output values into a single column vector
+    output = [firstValue; secondValue; thirdValue]; 
 
 end
